a1524af96cb2dd1cfc469957b668c5889916ced4
Justin Marsh (:flaviusb) <justinius.marsh@gmail.com>
2011-09-04 13:07:11 +1200

    Make building from a branch easier.

--- forceupdate.sh
+++ forceupdate.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 #ssh -t flaviusb@flaviusb.net "export PATH=$PATH:/home/flaviusb/code/ioke/bin/:/home/flaviusb/code/Byeloblog/:/home/flaviusb/bin:/var/lib/gems/1.8/bin/ && cd code/whyso.co.nz/ && git pull && byeloblog.ik && echo done"
-ssh -t flaviusb@flaviusb.net "export PATH=\"$PATH:/home/flaviusb/binaries/ioke/bin/:/home/flaviusb/code/Byeloblog/:/home/flaviusb/bin:/var/lib/gems/1.8/bin/\" && cd code/whyso.co.nz/ && git pull && byeloblog.ik && echo done" && kdialog --passivepopup "Finished building whyso.co.nz"
+ssh -t flaviusb@flaviusb.net "export PATH=\"$PATH:/home/flaviusb/binaries/ioke/bin/:/home/flaviusb/code/Byeloblog/:/home/flaviusb/bin:/var/lib/gems/1.8/bin/\" && cd code/whyso.co.nz/ && git pull && git checkout -f $1 && byeloblog.ik && echo done" && kdialog --passivepopup "Finished building whyso.co.nz"
